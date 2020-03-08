50e0f038f94bbe65dabb565c781d30442f221392
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2020-03-08 16:14:00 -0300

    Informação da página agora é exibida no App

--- lib/helpers/DataScrapper.dart
+++ lib/helpers/DataScrapper.dart
@@ -1,21 +1,24 @@
-import 'dart:convert';             // Contains the JSON encoder
+import 'dart:convert'; // Contains the JSON encoder
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';   // Contains a client for making API calls
+import 'package:http/http.dart'; // Contains a client for making API calls
 import 'package:html/parser.dart'; // Contains HTML parsers to generate a Document object
-import 'package:html/dom.dart' as dom;    // Contains DOM related classes for extracting data from elements
+import 'package:html/dom.dart'
+    as dom; // Contains DOM related classes for extracting data from elements
 
-class Meal{
-  String day,date,type;
+class Meal {
+  String day, date, type;
   List<String> lista;
-  Meal(this.date,this.day,this.type,this.lista);
+
+  Meal(this.date, this.day, this.type, this.lista);
+
   @override
   String toString() {
-    String listagem= "";
-    for(int i=0;i<lista.length;i++)
-      listagem+=lista[i]+'\n';
-    return this.date+"\n"+this.day+"\n"+this.type+"\n"+listagem;
+    String listagem = "";
+    for (int i = 0; i < lista.length; i++) listagem += lista[i] + '\n';
+    return this.date + "\n" + this.day + "\n" + this.type + "\n" + listagem;
   }
 }
+
 class DataScrapper {
   DataScrapper(String url) {
     this._url = url;
@@ -25,7 +28,7 @@ class DataScrapper {
   String _url;
 
   // Método para obter os dados
-  Future initiate() async {
+  Future<List<Meal>> initiate() async {
     var client = Client();
     Response response = await client.get(this._url);
 
@@ -33,43 +36,44 @@ class DataScrapper {
 
     print(document.querySelectorAll('#content'));
     meals = find(response);
-  //  print (meals.toString());
+    //  print (meals.toString());
     print(document.querySelectorAll("#cardapio").toString().length);
-
+    return meals;
   }
-  List<Meal> find(Response response){
+
+  List<Meal> find(Response response) {
     var s = response.body.split('<div id="cardapio">');
-    s[0]= "";
+    s[0] = "";
     s.removeAt(0);
     var aux = s.toString().replaceAll("<b>", "");
-    aux = aux.toString().replaceAll("</"+"b>", "");
+    aux = aux.toString().replaceAll("</" + "b>", "");
     aux = aux.toString().replaceAll("<div>", "");
-    aux = aux.toString().replaceAll("</"+"div>", "");
-    aux = aux.toString().replaceAll('<div class="cardapio_titulo">',"");
+    aux = aux.toString().replaceAll("</" + "div>", "");
+    aux = aux.toString().replaceAll('<div class="cardapio_titulo">', "");
     var ax;
-    do{
+    do {
       aux = aux.toString().replaceAll('  ', ' ');
       ax = aux.toString().replaceAll('  ', ' ');
-    } while(aux!=ax);
-    aux= aux.toString().replaceAll("\n \n \n","\n");
-    aux= aux.toString().replaceAll("\n - \n","\n");
-    aux= aux.toString().replaceAll("\n : \n","\n");
-    aux= aux.toString().replaceAll("<span>","");
-    aux= aux.toString().replaceAll("</span>","");
-    var as= aux.toString().split('</article>');
-    s= as.toString().split('<div class="col-lg-7 metade periodo">');
+    } while (aux != ax);
+    aux = aux.toString().replaceAll("\n \n \n", "\n");
+    aux = aux.toString().replaceAll("\n - \n", "\n");
+    aux = aux.toString().replaceAll("\n : \n", "\n");
+    aux = aux.toString().replaceAll("<span>", "");
+    aux = aux.toString().replaceAll("</span>", "");
+    var as = aux.toString().split('</article>');
+    s = as.toString().split('<div class="col-lg-7 metade periodo">');
     s.removeLast();
     s.removeAt(0);
     var refeicoes = List<Meal>(s.length);
-    for(int i=0;i<s.length;i++) {
+    for (int i = 0; i < s.length; i++) {
       var temp = s[i].split("\n");
-      for(int j=0;j<temp.length;j++){
-        if(temp[j].length<3){
+      for (int j = 0; j < temp.length; j++) {
+        if (temp[j].length < 3) {
           temp.removeAt(j);
-
         }
       }
-      refeicoes[i]= new Meal(temp[0], temp[1], temp[2],temp.sublist(3,temp.length-1));
+      refeicoes[i] =
+          new Meal(temp[0], temp[1], temp[2], temp.sublist(3, temp.length - 1));
     }
     return refeicoes;
   }
--- lib/ui/pagina_ru.dart
+++ lib/ui/pagina_ru.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ufscarplanner/helpers/DataScrapper.dart';
+
 class PaginaRu extends StatefulWidget {
   @override
   _PaginaRuState createState() => _PaginaRuState();
 }
+
 class _PaginaRuState extends State<PaginaRu> {
+  DataScrapper d1 = DataScrapper(
+      'https://www2.ufscar.br/restaurantes-universitario/cardapio');
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Página do RU"),
-      color: Colors.black45,
+    return FutureBuilder(
+      future: d1.initiate(),
+      builder: (context, snapshot) {
+        return Text(snapshot.data.toString());
+      },
     );
   }
 }
