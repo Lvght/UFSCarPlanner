793565c04d8c454ef612c6bcad2f058ba6b6a55e
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2020-03-08 06:58:21 -0300

    Correção dos arquivos vazios

---
+++ lib/ui/pagina_ru.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PaginaRu extends StatefulWidget {
+  @override
+  _PaginaRuState createState() => _PaginaRuState();
+}
+
+class _PaginaRuState extends State<PaginaRu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Pagina do RU"),
+      color: Colors.red,
+    );
+  }
+}
