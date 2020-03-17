10dd8b8f3d6c0fb41b885c319edf70cf9a8ad113
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2020-03-17 11:12:54 -0300

    Rota para página de notícia na aba respectiva
    Foi criado um GestureDetector para mover o usuário para uma outra página em que ele poderá ler a notícia.

---
+++ lib/ui/news_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NewsPage extends StatelessWidget {
+
+  NewsPage(this.titulo, this.autor, this.data, this.corpo);
+
+  String titulo;
+  String autor;
+  String data;
+  String corpo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.titulo),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("Autor: $autor"),
+          Text("Data: $data"),
+          Text(corpo)
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pagina_noticias.dart
+++ lib/ui/pagina_noticias.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart'; //
 import 'package:http/http.dart' as http;
+import 'package:ufscarplanner/ui/news_page.dart';
 
 class PaginaNoticias extends StatefulWidget {
   @override
@@ -22,43 +23,44 @@ class _PaginaNoticiaState extends State<PaginaNoticias> {
         .replaceAll("<td>", "TD")
         .replaceAll("<tr>", "TR")
         .replaceAll("href=", "")
-        .replaceAll("<a", "").replaceAll("</"+"a>","")
+        .replaceAll("<a", "")
+        .replaceAll("</" + "a>", "")
         .split("TR");
     Map<String, String> B = {"Data": "", "Link": "", "Titulo": ""};
     Map<String, String> E, C = B;
     List<String> A = ["Data", "Link", "Titulo"];
     List<Map<String, String>> D = new List<Map<String, String>>();
 
-   // print("s -   "+S.toString());
+    // print("s -   "+S.toString());
     for (int i = 0; i < S.length; i++) {
-      B =  {"Data": "", "Link": "", "Titulo": ""};
-      if(S[i]!="") {
+      B = {"Data": "", "Link": "", "Titulo": ""};
+      if (S[i] != "") {
         B[A[0]] = S[i].split("TD")[1];
         B[A[1]] = S[i].split("TD")[2].split('"')[1];
-        B[A[2]] = S[i].split("TD")[2].split('"')[2].replaceAll(">","");
+        B[A[2]] = S[i].split("TD")[2].split('"')[2].replaceAll(">", "");
         E = await LoadNews(B["Link"]);
         B.addAll(E);
-        var F=B;
-        do{
-          F=B;
+        var F = B;
+        do {
+          F = B;
           B[A[0]].replaceAll("  ", " ");
           B[A[1]].replaceAll("  ", " ");
           B[A[2]].replaceAll("  ", " ");
           B["Autor"].replaceAll("  ", " ");
           B["Texto"].replaceAll("  ", " ");
-        }while(B!=F);
-        if (B !=  {"Data": "", "Link": "", "Titulo": ""}) {
+        } while (B != F);
+        if (B != {"Data": "", "Link": "", "Titulo": ""}) {
           D.add(B);
-        }else{
-
-          debugPrint("B: "+B.toString());
-          debugPrint("C :"+C.toString());
+        } else {
+          debugPrint("B: " + B.toString());
+          debugPrint("C :" + C.toString());
         }
-       debugPrint(B.toString());
+        // debugPrint(B.toString());
       }
     }
     //TODO CHAMAR DECENTEMENTE ESSA FUNÇÃO
     //TODO ONSTRUIR OS WIDGETS
+    debugPrint(D.toString());
     return await D;
   }
 
@@ -66,8 +68,7 @@ class _PaginaNoticiaState extends State<PaginaNoticias> {
     http.Response response = await http.get(url);
 
     Map<String, String> E = {"Autor": "", "Texto": ""};
-    E["Autor"] =
-      response.body.split('<div class="autor">')[1].split("</" + "i>")[0].replaceAll("<i>", "").replaceAll("\n", "");
+    E["Autor"] = response.body.split('<div class="autor">')[1].split("</" + "i>")[0].replaceAll("<i>", "").replaceAll("\n", "");
     var aux = response.body
         .split('<div class="texto">')[1]
         .split("</" + "div>")[0]
@@ -78,40 +79,51 @@ class _PaginaNoticiaState extends State<PaginaNoticias> {
         .replaceAll("</" + "strong>", "")
         .replaceAll("</" + "em>", "")
         .replaceAll("<u>", "")
-        .replaceAll("</u>","")
+        .replaceAll("</u>", "")
         .replaceAll('<a href="', "<sploint>")
-        .replaceAll("</a>", "").split("<sploint>");
-    var ax="";
-    for(int i =0 ;i<aux.length;i++) {
+        .replaceAll("</a>", "")
+        .split("<sploint>");
+    var ax = "";
+    for (int i = 0; i < aux.length; i++) {
       if (aux[i].contains("<a")) {
-        ax+=aux[i].split("<a")[0];
-        for(int j=1;j<aux[i].split("<a")[1].split(">").length;j++)
-          ax+=aux[i].split("<a")[1].split(">")[j];
-
-      }else{
-        ax+=aux[i];
+        ax += aux[i].split("<a")[0];
+        for (int j = 1; j < aux[i].split("<a")[1].split(">").length; j++) ax += aux[i].split("<a")[1].split(">")[j];
+      } else {
+        ax += aux[i];
       }
     }
-    E["Texto"]=ax;
+    E["Texto"] = ax;
 
     return E;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height ,
-      width: MediaQuery.of(context).size.width ,
-      child: RaisedButton(
-        child: Text("GIANT\nBUTTON\nprototipo"),
-        onPressed: (){
-          getLinks('https://www2.ufscar.br/noticias');
-        },
-      )
-
-      //TODO FAZER NAVIGATION BAR DA SEMANA
-      //TODO FAZER A ORGANIZAÇÃO DE HORARIOS
-      //TODO CARREGAR DADOS DE B,SE EXISTENTES
-    );
+    return FutureBuilder(
+        future: getLinks('https://www2.ufscar.br/noticias'),
+        builder: (BuildContext context, AsyncSnapshot<List<Map<String, String>>> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            default:
+              return ListView.builder(itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => NewsPage(
+                                snapshot.data[index]["Titulo"],
+                                snapshot.data[index]["Autor"].trim(),
+                                snapshot.data[index]["Data"].trim(),
+                                snapshot.data[index]["Texto"],
+                              ))),
+                  child: ListTile(title: Text(snapshot.data[index]["Titulo"])),
+                );
+              });
+          }
+        });
   }
 }
