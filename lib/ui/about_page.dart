1b6187e88b1cfaa5306e4610314528bc35f7dd93
Vinicius Quaresma da Luz <28585648+vinql@users.noreply.github.com>
2020-03-28 15:37:56 -0300

    Adicionado botão para perfil do Twitter

--- lib/ui/about_page.dart
+++ lib/ui/about_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatefulWidget {
   @override
@@ -36,14 +37,30 @@ class _AboutPageState extends State<AboutPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text("Matheus Ramos de Carvalho", style: TextStyle(fontSize: 22),),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: Colors.redAccent,
-                          ),
-                          padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
-                          child: Text("BCC 19", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-                        )
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Image.asset("_assets/twitter.png", height: 20,),
+                            SizedBox(width: 5,),
+                            FlatButton(
+                              onPressed: () async => await launch("https://twitter.com/oak_branches"),
+                              padding: EdgeInsets.all(0),
+                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              child: Text("@oak_branchs", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.red),),
+                            ),
+                            SizedBox(width: 10,),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                color: Colors.redAccent,
+                              ),
+                              padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+                              child: Text("BCC 19", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+                            )
+                          ],
+                        ),
                       ],
                     ),
                     SizedBox(height: 20,),
@@ -51,14 +68,30 @@ class _AboutPageState extends State<AboutPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text("Vinicius Quaresma da Luz", style: TextStyle(fontSize: 22),),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: Colors.redAccent,
-                          ),
-                          padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
-                          child: Text("BCC 19", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-                        )
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Image.asset("_assets/twitter.png", height: 20,),
+                            SizedBox(width: 5,),
+                            FlatButton(
+                              onPressed: () async => await launch("https://twitter.com/vncsqrsm"),
+                              padding: EdgeInsets.all(0),
+                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              child: Text("@vncsqrsm", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.red),),
+                            ),
+                            SizedBox(width: 10,),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                color: Colors.redAccent,
+                              ),
+                              padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+                              child: Text("BCC 19", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+                            )
+                          ],
+                        ),
                       ],
                     )
                   ],
